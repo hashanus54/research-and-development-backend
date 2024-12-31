c1eb42ca47ef7885908ddb0328219b960705ea2c
Tasheela Jayawickrama <tasheelajay1999@gmail.com>
2024-12-31 21:06:04 +0530

    changes routes file

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
 const router = express.Router();
+const authorized = require('../middleware/AuthMiddleware.js');
+
 
 router.post('/sign-up', UserController.signUp);
-router.post('/sign-up-with-message', UserController.signUpWithMessage);
 router.post('/sign-in', UserController.signIn);
 router.post('/forgot-password', UserController.forgotPassword);
 router.post('/reset-password', UserController.resetPassword);
-router.post('/verify-email/:verificationToken', UserController.verifyUser);
-router.post('/resend-email-verification', UserController.resendVerificationEmail);
+router.post('/verify-email', UserController.verifyEmailWithOtp);
+router.post('/verify-mobile', UserController.verifyPhoneWithOtp);
+router.post('/resend-otp', UserController.resendOTP);
+router.post('/create-director', authorized(['SUPER_ADMIN', 'ADMIN']), UserController.createDirector);
+router.put('/update-user/:id', authorized(['SUPER_ADMIN']), UserController.updateUser);
+router.put('/update-user-role/:id', authorized(['SUPER_ADMIN']), UserController.updateUserRole);
+router.delete('/delete-user/:id', authorized(['SUPER_ADMIN']), UserController.deleteUser);
 
 module.exports = router;
